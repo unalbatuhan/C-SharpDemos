4a5cb3e961c11984aa226470bc546f9ebc4d9ee0
Batuhan UNAL <unal.batuhan@outlook.com>
2016-10-18 22:05:10 +0300

    Commit
    CS sayının karesini hesaplayan App.1

---
+++ Soru1/Soru1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soru1
+{
+
+    class Program
+    {
+        //static int kareal(int a)
+        //{
+
+        //    //int karesi = a * a;
+        //    //return karesi;
+
+        //}
+        static void Main(string[] args)
+        {
+            Console.Write("Lütfen bir sayı giriniz :  ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            int sonuc = x * x;
+            Console.Write(sonuc);
+
+            Console.ReadLine();
+
+           
+
+        }
+
+        //private static int KareAl(int sayı)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+
+
+
+        //private static bool kareal(double v)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+
+        //private static bool kareal(double v)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public  bool kareal(int s)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+    }
+}
+
