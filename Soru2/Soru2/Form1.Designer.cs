760ad82ab90a2c5a8c6d7a10d5d04e1f64aca4e7
Batuhan UNAL <unal.batuhan@outlook.com>
2016-10-18 21:59:22 +0300

    Comment
    CS Hesap makinesi Uygulaması 
    4 işlem yerine getirildi.

---
+++ Soru2/Soru2/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Soru2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblSayi1 = new System.Windows.Forms.Label();
+            this.lblSayi2 = new System.Windows.Forms.Label();
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.txtSayi1 = new System.Windows.Forms.TextBox();
+            this.txtSayi2 = new System.Windows.Forms.TextBox();
+            this.txtSonuc = new System.Windows.Forms.TextBox();
+            this.btnTopla = new System.Windows.Forms.Button();
+            this.btnCikar = new System.Windows.Forms.Button();
+            this.btnCarp = new System.Windows.Forms.Button();
+            this.btnBol = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // lblSayi1
+            // 
+            this.lblSayi1.AutoSize = true;
+            this.lblSayi1.Location = new System.Drawing.Point(69, 56);
+            this.lblSayi1.Name = "lblSayi1";
+            this.lblSayi1.Size = new System.Drawing.Size(45, 13);
+            this.lblSayi1.TabIndex = 0;
+            this.lblSayi1.Text = "1. Sayı :";
+            // 
+            // lblSayi2
+            // 
+            this.lblSayi2.AutoSize = true;
+            this.lblSayi2.Location = new System.Drawing.Point(69, 108);
+            this.lblSayi2.Name = "lblSayi2";
+            this.lblSayi2.Size = new System.Drawing.Size(45, 13);
+            this.lblSayi2.TabIndex = 1;
+            this.lblSayi2.Text = "2. Sayı :";
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.AutoSize = true;
+            this.lblSonuc.Location = new System.Drawing.Point(69, 166);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(44, 13);
+            this.lblSonuc.TabIndex = 1;
+            this.lblSonuc.Text = "Sonuç :";
+            // 
+            // txtSayi1
+            // 
+            this.txtSayi1.Location = new System.Drawing.Point(138, 53);
+            this.txtSayi1.Name = "txtSayi1";
+            this.txtSayi1.Size = new System.Drawing.Size(113, 20);
+            this.txtSayi1.TabIndex = 2;
+            // 
+            // txtSayi2
+            // 
+            this.txtSayi2.Location = new System.Drawing.Point(138, 108);
+            this.txtSayi2.Name = "txtSayi2";
+            this.txtSayi2.Size = new System.Drawing.Size(113, 20);
+            this.txtSayi2.TabIndex = 3;
+            // 
+            // txtSonuc
+            // 
+            this.txtSonuc.Location = new System.Drawing.Point(138, 166);
+            this.txtSonuc.Name = "txtSonuc";
+            this.txtSonuc.Size = new System.Drawing.Size(113, 20);
+            this.txtSonuc.TabIndex = 4;
+            // 
+            // btnTopla
+            // 
+            this.btnTopla.ImageKey = "imgtopla.png";
+            this.btnTopla.ImageList = this.imageList1;
+            this.btnTopla.Location = new System.Drawing.Point(328, 51);
+            this.btnTopla.Name = "btnTopla";
+            this.btnTopla.Size = new System.Drawing.Size(84, 45);
+            this.btnTopla.TabIndex = 5;
+            this.btnTopla.UseVisualStyleBackColor = true;
+            this.btnTopla.Click += new System.EventHandler(this.btnTopla_Click);
+            // 
+            // btnCikar
+            // 
+            this.btnCikar.ImageKey = "imgcikar.png";
+            this.btnCikar.ImageList = this.imageList1;
+            this.btnCikar.Location = new System.Drawing.Point(469, 51);
+            this.btnCikar.Name = "btnCikar";
+            this.btnCikar.Size = new System.Drawing.Size(87, 45);
+            this.btnCikar.TabIndex = 5;
+            this.btnCikar.UseVisualStyleBackColor = true;
+            this.btnCikar.Click += new System.EventHandler(this.btnCikar_Click);
+            // 
+            // btnCarp
+            // 
+            this.btnCarp.ImageKey = "imgcarpma.png";
+            this.btnCarp.ImageList = this.imageList1;
+            this.btnCarp.Location = new System.Drawing.Point(328, 150);
+            this.btnCarp.Name = "btnCarp";
+            this.btnCarp.Size = new System.Drawing.Size(84, 45);
+            this.btnCarp.TabIndex = 5;
+            this.btnCarp.UseVisualStyleBackColor = true;
+            this.btnCarp.Click += new System.EventHandler(this.btnCarp_Click);
+            // 
+            // btnBol
+            // 
+            this.btnBol.ImageKey = "imgbolme.png";
+            this.btnBol.ImageList = this.imageList1;
+            this.btnBol.Location = new System.Drawing.Point(469, 150);
+            this.btnBol.Name = "btnBol";
+            this.btnBol.Size = new System.Drawing.Size(87, 48);
+            this.btnBol.TabIndex = 5;
+            this.btnBol.UseVisualStyleBackColor = true;
+            this.btnBol.Click += new System.EventHandler(this.btnBol_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "imgbolme.png");
+            this.imageList1.Images.SetKeyName(1, "imgcarpma.png");
+            this.imageList1.Images.SetKeyName(2, "imgcikar.png");
+            this.imageList1.Images.SetKeyName(3, "imgtopla.png");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 336);
+            this.Controls.Add(this.btnBol);
+            this.Controls.Add(this.btnCarp);
+            this.Controls.Add(this.btnCikar);
+            this.Controls.Add(this.btnTopla);
+            this.Controls.Add(this.txtSonuc);
+            this.Controls.Add(this.txtSayi2);
+            this.Controls.Add(this.txtSayi1);
+            this.Controls.Add(this.lblSonuc);
+            this.Controls.Add(this.lblSayi2);
+            this.Controls.Add(this.lblSayi1);
+            this.Name = "Form1";
+            this.Text = "Hesap Makinesi";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSayi1;
+        private System.Windows.Forms.Label lblSayi2;
+        private System.Windows.Forms.Label lblSonuc;
+        private System.Windows.Forms.TextBox txtSayi1;
+        private System.Windows.Forms.TextBox txtSayi2;
+        private System.Windows.Forms.TextBox txtSonuc;
+        private System.Windows.Forms.Button btnTopla;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Button btnCikar;
+        private System.Windows.Forms.Button btnCarp;
+        private System.Windows.Forms.Button btnBol;
+    }
+}
+
---
+++ Soru2/Soru2/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Soru2
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            txtSonuc.Enabled = false;
+           
+        }
+
+        private void btnTopla_Click(object sender, EventArgs e)
+        {
+            int x = Convert.ToInt32(txtSayi1.Text);
+            int y = Convert.ToInt32(txtSayi2.Text);
+            txtSonuc.Text = Convert.ToString(x + y);
+        }
+
+        private void btnCikar_Click(object sender, EventArgs e)
+        {
+            int x = Convert.ToInt32(txtSayi1.Text);
+            int y = Convert.ToInt32(txtSayi2.Text);
+            txtSonuc.Text = Convert.ToString(x - y);
+        }
+
+        private void btnCarp_Click(object sender, EventArgs e)
+        {
+            int x = Convert.ToInt32(txtSayi1.Text);
+            int y = Convert.ToInt32(txtSayi2.Text);
+            txtSonuc.Text = Convert.ToString(x * y);
+        }
+
+        private void btnBol_Click(object sender, EventArgs e)
+        {
+            int x = Convert.ToInt32(txtSayi1.Text);
+            int y = Convert.ToInt32(txtSayi2.Text);
+            txtSonuc.Text = Convert.ToString(x / y);
+        }
+    }
+}
