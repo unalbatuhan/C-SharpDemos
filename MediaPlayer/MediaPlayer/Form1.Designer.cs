b4655cd90f5ae1da84d91948025e4655c92e058b
Batuhan UNAL <batuhanunal@ogr.halic.edu.tr>
2017-03-15 03:19:50 +0300

    Summary
    Sample Media Player demo app

---
+++ MediaPlayer/MediaPlayer/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace MediaPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.btn_Oynat = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(0, 1);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(351, 305);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            // 
+            // btn_Oynat
+            // 
+            this.btn_Oynat.Location = new System.Drawing.Point(408, 270);
+            this.btn_Oynat.Name = "btn_Oynat";
+            this.btn_Oynat.Size = new System.Drawing.Size(75, 23);
+            this.btn_Oynat.TabIndex = 1;
+            this.btn_Oynat.Text = "Oynat";
+            this.btn_Oynat.UseVisualStyleBackColor = true;
+            this.btn_Oynat.Click += new System.EventHandler(this.btn_Oynat_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(367, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(472, 242);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(837, 349);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btn_Oynat);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Name = "Form1";
+            this.Text = "BatuSoft - MediaPlayer ver. 1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Button btn_Oynat;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ MediaPlayer/MediaPlayer/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MediaPlayer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Assembly assembly = Assembly.GetExecutingAssembly();
+            //Stream stream = assembly.GetManifestResourceStream("MediaPlayer.Resources.AMORPHIS - Alone.mp3");
+            //StreamReader s = new StreamReader(stream);
+        }
+
+        private void btn_Oynat_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "C:\\Users\\Batuhan\\Music\\AMORPHIS - Alone.mp3";            
+            pictureBox1.ImageLocation = "C:\\Users\\Batuhan\\Pictures\\docker-wallpaper-black.jpg";
+        }
+    }
+}
